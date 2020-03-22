c7230a171ff44fd1a5d56d5798f6e0ca0bf069fd
Alexandr Kondratiev <ghoostt61@gmail.com>
2020-03-22 21:04:32 +0400

    feat: add solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let lengthStr = str.length;
+
+    bracketsConfig = bracketsConfig.map(element => {
+        return element.join("");
+    });
+
+    for (let i = 0; i < lengthStr; i += 2) {
+        for (let j = 0; j < bracketsConfig.length; j++)
+            str = str.replace(bracketsConfig[j], "");
+        if (str.length == 0) return true;
+    }
+
+    return false;
+};
